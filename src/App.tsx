1edc36419a3f227a7dceb7f67d122dba94d4806d
BauBau26 <110334971+BauBau26@users.noreply.github.com>
2022-12-17 16:21:45 +0000

    move button to own file

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { FunctionComponent, ReactNode } from "react";
 import "./App.css";
+import { Button } from "./Button";
 
 function App() {
   return (
@@ -43,36 +43,5 @@ function ButtonBar() {
     </div>
   );
 }
-
-interface ButtonProps {
-  text: string;
-  isGreen?: boolean;
-  icon?: ReactNode;
-}
-
-const Button: FunctionComponent<ButtonProps> = (props) => {
-  // if button "is green" then use the green button class, otherwise use the normal one
-  const buttonClass = props.isGreen ? "green-button" : "normal-button";
-
-  return (
-    <button className={`all-buttons ${buttonClass} icon-button`}>
-      {props.icon !== undefined && (
-        // ^ if left statement is false, then stuff after && isn't run and we don't
-        // get ime icon in the page
-        <svg
-          height="16"
-          viewBox="0 0 16 16"
-          version="1.1"
-          width="16"
-          data-view-component="true"
-          className="icon-branch"
-        >
-          {props.icon}
-        </svg>
-      )}
-      {props.text}
-    </button>
-  );
-};
 
 export default App;
---
+++ src/Button.tsx
@@ -0,0 +1,32 @@
+import { FunctionComponent, ReactNode } from "react";
+
+interface ButtonProps {
+  text: string;
+  isGreen?: boolean;
+  icon?: ReactNode;
+}
+
+export const Button: FunctionComponent<ButtonProps> = (props) => {
+  // if button "is green" then use the green button class, otherwise use the normal one
+  const buttonClass = props.isGreen ? "green-button" : "normal-button";
+
+  return (
+    <button className={`all-buttons ${buttonClass} icon-button`}>
+      {props.icon !== undefined && (
+        // ^ if left statement is false, then stuff after && isn't run and we don't
+        // get ime icon in the page
+        <svg
+          height="16"
+          viewBox="0 0 16 16"
+          version="1.1"
+          width="16"
+          data-view-component="true"
+          className="icon-branch"
+        >
+          {props.icon}
+        </svg>
+      )}
+      {props.text}
+    </button>
+  );
+};
