0afc2c259ef8a22de9f66332443095b86c222aaf
BauBau26 <110334971+BauBau26@users.noreply.github.com>
2023-01-14 14:42:10 +0000

    stretched boxes

--- src/AddFileDialog.tsx
+++ src/AddFileDialog.tsx
@@ -14,23 +14,26 @@ export const AddFileDialog: FunctionComponent<AddFileDialogProps> = (props) => {
         height: "300px",
         display: "flex",
         flexDirection: "column",
+        gap: "20px",
       }}
       open={props.open}
     >
-      <div style={{ display: "flex", flexDirection: "column", flex: "1" }}>
+      <div style={{ display: "flex", flexDirection: "column", flex: 1 }}>
         <div style={{ display: "flex", justifyContent: "space-between" }}>
           Add file <button onClick={props.onClose}>X</button>
         </div>
-        <div>
+        <div style={{ display: "flex" }}>
           filename
-          <input />
+          <input style={{ flex: 1 }} />
         </div>
-        <div style={{ display: "flex", flexDirection: "column", flex: "1" }}>
+        <div style={{ display: "flex", flexDirection: "column", flex: 1 }}>
           contents
-          <textarea style={{ flex: "1" }} />
+          <textarea style={{ flex: 1 }} />
         </div>
-        <div>
-          <button>Ok</button>
+        <div style={{ display: "flex" }}>
+          Message
+          <input style={{ flex: 1 }} />
+          <button>Commit</button>
         </div>
       </div>
     </dialog>
